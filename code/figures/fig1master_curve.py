bc62d1f530a6efb9966b8a6a214d07541fcff2c8
Muir Morrison <muir@caltech.edu>
2020-05-16 00:42:45 -0500

    Finished discussion of means + other small edits

--- code/figures/fig1master_curve.py
+++ code/figures/fig1master_curve.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import srep.viz
 colors = srep.viz.color_selector('pboc')
-srep.viz.plotting_style()
+srep.viz.plotting_style(grid=False)
 
 fig, ax = plt.subplots(1, 1, figsize=(5, 3), dpi=150)
 x = np.linspace(-6,6)
